6aa8443970e58edcb792949965faa92d106cbd66
yours7himanshu <dinkarhimanshu78@gmail.com>
2023-10-05 11:00:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function videoconAnimation(){
+    const videocon=document.querySelector("#video-container");
+const playbtn = document.querySelector("#play")
+videocon.addEventListener("mouseenter",function(){
+gsap.to(playbtn,{
+    scale:1,
+    opacity:1,
+})
+})
+videocon.addEventListener("mouseleave",function(){
+    gsap.to(playbtn,{
+        scale:0,
+        opacity:0,
+    })
+})
+videocon.addEventListener("mousemove",function(dets){
+    gsap.to(playbtn,{
+        left:dets.x-50,
+        top:dets.y-80,
+    })
+})
+}
+videoconAnimation();
+
+
+function loadingAnimation(){
+
+    gsap.from("#page1 h1",{
+        y:30,
+        opacity:0,
+        delay:0.7,
+        duration:0.9,
+        stagger:0.3,
+    })
+    gsap.from("#page1 #video-conatiner",{
+        scale:0.6,
+        opacity:0,
+        delay:1.3,
+        duration:0.8,
+    })
+}
+loadingAnimation();
